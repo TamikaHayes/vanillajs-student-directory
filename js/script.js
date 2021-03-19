aea949eaec3d16ca2a34420eea41bd81f6187524
TamikaHayes <tamika.hayes@gmail.com>
2021-03-18 22:24:23 -0700

    Added showPage function

--- js/script.js
+++ js/script.js
@@ -12,12 +12,41 @@ For assistance:
 */
 
 
+'use strict';
 
+const perPage = 9;
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage(list, page) {
+   const startIndex = (page * perPage) - perPage;
+   const endIndex = (page * perPage);
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+      for (let i = 0; i < list.length; i += 1) {
+         if ( i >= startIndex && i < endIndex) {
+            let studentItem = list[i];
+            
+            const html = `
+         <li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="${studentItem.picture.large}" alt="Profile Picture">
+           <h3>${studentItem.name.first} ${studentItem.name.last}</h3>
+           <span class="email">${studentItem.email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">${studentItem.registered.date}</span>
+         </div>
+       </li>
+       `;
+       studentList.insertAdjacentHTML('beforeend', html);
+      }    
+    }
+}
+
+showPage(data, 1);
 
 
 /*
